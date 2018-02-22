be20ecea72222827c1e6e76632cb991ff44fbf78
SergioSSJ <sjcmexr@gmail.com>
2018-02-21 22:52:39 -0600

    react router integrated

--- src/main/front/src/index.js
+++ src/main/front/src/index.js
@@ -5,24 +5,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { ConnectedRouter } from 'react-router-redux'
-import { store} from "./store/store";
-import { Router, Route, browserHistory } from 'react-router'
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
-
-
-
-
-const history = syncHistoryWithStore(browserHistory, store)
-
+import { Route, Switch } from 'react-router-dom';
+import { ConnectedRouter } from "react-router-redux";
+import { store,history } from "./store/store";
+import { Router,browserHistory } from "react-router";
+import { syncHistoryWithStore, routerReducer } from "react-router-redux";
+import About from "./components/About";
 
 
 ReactDOM.render(
-
   <Provider store={store}>
-    <Router history={history}>
-    <App />
-    </Router>
+  <ConnectedRouter history={history}>
+    <Switch>      
+      <Route exact path="/" component={App} />
+      <Route exact path="/about" component={About} />
+    </Switch>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
 );
--- src/main/front/src/store/store.js
+++ src/main/front/src/store/store.js
@@ -1,14 +1,15 @@
 // store.js
 
 import { reducers } from "../reducers/reducer";
-import { createStore } from "redux";
+import { combineReducers, createStore, compose, applyMiddleware } from "redux";
 import createHistory from 'history/createBrowserHistory'
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
 
 export const history = createHistory()
 
 
 export function configureStore(initialState = {}) {
-  const store = createStore(reducers);
+  const store = createStore(reducers,undefined,compose(applyMiddleware(routerMiddleware(history))));
   console.log('store')
   return store;
 }
